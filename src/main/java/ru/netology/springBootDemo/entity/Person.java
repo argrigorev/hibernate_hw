67d63f400dda5262ce1ff8bf28d6a288cdc9cace
artem <artemkag04@gmail.com>
2025-08-15 17:15:18 +0300

    Added column for mapping

--- src/main/java/ru/netology/springBootDemo/entity/Person.java
+++ src/main/java/ru/netology/springBootDemo/entity/Person.java
@@ -1,5 +1,6 @@
 package ru.netology.springBootDemo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,7 +12,10 @@ public class Person {
     @EmbeddedId
     private PersonId personId;
 
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "city_of_living")
     private String cityOfLiving;
 
     public Person() {}
