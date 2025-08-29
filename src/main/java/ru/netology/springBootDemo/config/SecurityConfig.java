3df756dc518391b3bd26b07477af085653f262b5
artem <artemkag04@gmail.com>
2025-08-30 01:00:39 +0300

    add changes

--- src/main/java/ru/netology/springBootDemo/config/SecurityConfig.java
+++ src/main/java/ru/netology/springBootDemo/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.netology.springBootDemo;
+package ru.netology.springBootDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
